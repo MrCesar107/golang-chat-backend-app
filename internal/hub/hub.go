9a0daf86c0a7fc1ce4dcf6349c5169ace2f9cd39
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2025-06-19 20:43:01 -0600

    Create Hub logic

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,1 +1,35 @@
 package hub
+
+import "chatbackendapp/internal/common"
+
+func NewHub() *common.Hub {
+	return &common.Hub{
+		Clients: make(map[*common.Client]bool),
+		Broadcast: make(chan []byte),
+		Register: make(chan *common.Client),
+		Unregister: make(chan *common.Client),
+	}
+}
+
+func Run(h *common.Hub) {
+	for {
+		select {
+		case client := <- h.Register:
+			h.Clients[client] = true
+		case client := <- h.Unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.Send)
+			}
+		case message := <- h.Broadcast:
+			for client := range h.Clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.Clients, client)
+				}
+			}
+		}
+	}
+}
